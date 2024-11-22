d013c1239cd7c32e93dd5df72523deece8a60c8d
pipeb <pipeba1408@gmail.com>
2024-11-22 05:05:54 -0500

    Add CORS

--- src/main/java/com/aerolinea/updates/UpdateController.java
+++ src/main/java/com/aerolinea/updates/UpdateController.java
@@ -1,10 +1,12 @@
 package com.aerolinea.updates;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = { "*" })
 public class UpdateController {
 
     private final UpdateService updateService;
